0ceba4b050912224d5beb5fe30dca37636f94626
danielgek <danielgek@gmail.com>
2015-10-14 20:36:41 +0100

    add method's to de interface

--- src/rmi/StorageServerInterface.java
+++ src/rmi/StorageServerInterface.java
@@ -1,8 +1,6 @@
 package rmi;
 
-import models.Project;
-import models.Response;
-import models.User;
+import models.*;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
@@ -12,8 +10,16 @@ import java.util.Date;
  * Created by danielgek on 05/10/15.
  */
 public interface StorageServerInterface extends Remote{
-    public Response register(User user) throws RemoteException;
-    public Response login(User user) throws RemoteException;
-    public Response createProject(Project project) throws RemoteException;
-    public Response getProject(int id) throws RemoteException;
+    Response register(User user) throws RemoteException;
+    Response login(User user) throws RemoteException;
+    Response createProject(Project project) throws RemoteException;
+    Response getProject(int id) throws RemoteException;
+    Response createReward(Reward reward) throws RemoteException;
+    Response getRewards(int projectId) throws RemoteException;
+    Response createLevel(Level level) throws RemoteException;
+    Response getLevels(int projectId) throws RemoteException;
+    Response createPoll(Poll poll) throws RemoteException;
+    Response getPolls(int projectId) throws RemoteException;
+    Response createVote(Vote vote) throws RemoteException;
+    Response getVotes(int pollId) throws RemoteException;
 }
