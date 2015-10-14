c6016a2edc431db390f7a1912f1d0200d00d6d78
danielgek <danielgek@gmail.com>
2015-10-14 20:36:18 +0100

    add database querys and some fixes

--- src/rmi/StorageServer.java
+++ src/rmi/StorageServer.java
@@ -3,14 +3,13 @@ package rmi;
 
 
 import Util.Debug;
-import models.Project;
-import models.Response;
-import models.User;
+import models.*;
 
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.Date;
 
 /**
@@ -61,7 +60,7 @@ public class StorageServer extends UnicastRemoteObject implements StorageServerI
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        System.out.println("Servidor RMI ligado!");
+        Debug.m("Servidor RMI ligado!");
 
     }
 
@@ -69,7 +68,7 @@ public class StorageServer extends UnicastRemoteObject implements StorageServerI
 
     @Override
     public Response register(User user) throws RemoteException {
-        Debug.message("Start registering User");
+        Debug.m("Start registering User" + user.toString());
         try {
 
             rs = statement.executeQuery("SELECT * FROM users WHERE user = '" + user.getUsername() +"';");
@@ -87,9 +86,9 @@ public class StorageServer extends UnicastRemoteObject implements StorageServerI
         }
         catch (SQLException ex){
             // handle any errors
-            System.out.println("SQLException: " + ex.getMessage());
-            System.out.println("SQLState: " + ex.getSQLState());
-            System.out.println("VendorError: " + ex.getErrorCode());
+            Debug.m("SQLException: " + ex.getMessage());
+            Debug.m("SQLState: " + ex.getSQLState());
+            Debug.m("VendorError: " + ex.getErrorCode());
             return new Response(false, "Error on inserting on database");
         }
         finally {
@@ -110,7 +109,7 @@ public class StorageServer extends UnicastRemoteObject implements StorageServerI
 
     @Override
     public Response login(User user) throws RemoteException {
-        Debug.message("Starting Login");
+        Debug.m("Starting Login" + user.toString());
         try {
 
             rs = statement.executeQuery("SELECT * FROM users WHERE username = '" + user.getUsername() + "' AND password = '" + user.getPassword() + "';");
@@ -118,16 +117,16 @@ public class StorageServer extends UnicastRemoteObject implements StorageServerI
             if(user.getUsername() == rs.getString("user")){
                 return new Response(true, "Logged in successfuly");
             }else{
-                return new Response(false,"User cardentials wrong!");
+                return new Response(false, "User cardentials wrong!");
             }
 
 
         }
         catch (SQLException ex){
             // handle any errors
-            System.out.println("SQLException: " + ex.getMessage());
-            System.out.println("SQLState: " + ex.getSQLState());
-            System.out.println("VendorError: " + ex.getErrorCode());
+            Debug.m("SQLException: " + ex.getMessage());
+            Debug.m("SQLState: " + ex.getSQLState());
+            Debug.m("VendorError: " + ex.getErrorCode());
             return new Response(false, "Error on login on database");
         }
         finally {
@@ -148,11 +147,374 @@ public class StorageServer extends UnicastRemoteObject implements StorageServerI
 
     @Override
     public Response createProject(Project project) throws RemoteException {
-        return null;
+
+        Debug.m("Creating project! " + project.toString());
+
+        try {
+
+            statement.executeUpdate("INSERT INTO projects (id_admin, title, description, objective, limit)" +
+                    "VALUES" +
+                    "(" + project.getAdminId() + ", '" +
+                    project.getName() + "', '" +
+                    project.getDescription() + "', " +
+                    project.getObjective() + ", '" +
+                    project.getLimit() + "');");
+
+            rs=statement.getGeneratedKeys();
+
+            Response response = new Response(true, "Project inserted successfully!");
+
+            if (rs.next()){
+                response.setObject(new Project(
+                        rs.getInt(1),
+                        rs.getInt(2),
+                        rs.getString(3),
+                        rs.getString(4),
+                        rs.getDouble(5),
+                        rs.getDate(6)));
+            }
+
+            return response;
+        }
+        catch (SQLException ex){
+            // handle any errors
+            Debug.m("SQLException: " + ex.getMessage());
+            Debug.m("SQLState: " + ex.getSQLState());
+            Debug.m("VendorError: " + ex.getErrorCode());
+            return new Response(false, "Error on Project insertion");
+        }
+        finally {
+            // cleaning stuff
+            if (rs != null) {
+                try {rs.close();} catch (SQLException sqlEx) { } // ignore
+
+                rs = null;
+            }
+
+            if (statement != null) {
+                try {statement.close();} catch (SQLException sqlEx) { } // ignore
+
+                statement = null;
+            }
+        }
     }
 
     @Override
     public Response getProject(int id) throws RemoteException {
-        return null;
+        try {
+            rs = statement.executeQuery("SELECT * FROM projects WHERE id = " + id + ";");
+
+            Response response = new Response(true, "There is your project!");
+
+            if(rs.next()){
+                response.setObject(new Project(
+                        rs.getInt(1),
+                        rs.getInt(2),
+                        rs.getString(3),
+                        rs.getString(4),
+                        rs.getDouble(5),
+                        rs.getDate(6)));
+            }
+
+            return response;
+
+        } catch (SQLException e) {
+            return new Response(false, "Can't find that project!");
+        }
+    }
+
+    @Override
+    public Response createReward(Reward reward) throws RemoteException {
+        Debug.m("Creating reward! " + reward.toString());
+
+        try {
+
+            statement.executeUpdate("INSERT INTO rewards (id_project, description, value)" +
+                    "VALUES" +
+                    "(" + reward.getProjectId() + ", '" +
+                    reward.getDescription() + "', " +
+                    reward.getValue() + ");");
+
+            rs=statement.getGeneratedKeys();
+
+            Response response = new Response(true, "Reward inserted successfully!");
+
+            if (rs.next()){
+                response.setObject(new Reward(
+                        rs.getInt(1),
+                        rs.getInt(2),
+                        rs.getString(3),
+                        rs.getDouble(4)));
+            }
+
+            return response;
+        }
+        catch (SQLException ex){
+            // handle any errors
+            Debug.m("SQLException: " + ex.getMessage());
+            Debug.m("SQLState: " + ex.getSQLState());
+            Debug.m("VendorError: " + ex.getErrorCode());
+            return new Response(false, "Error on Reward insertion");
+        }
+        finally {
+            // cleaning stuff
+            if (rs != null) {
+                try {rs.close();} catch (SQLException sqlEx) { } // ignore
+
+                rs = null;
+            }
+
+            if (statement != null) {
+                try {statement.close();} catch (SQLException sqlEx) { } // ignore
+
+                statement = null;
+            }
+        }
+    }
+
+    @Override
+    public Response getRewards(int projectId) throws RemoteException {
+        try {
+            rs = statement.executeQuery("SELECT * FROM rewards WHERE id_project = " + projectId + ";");
+
+            Response response = new Response(true, "There is your reward!");
+
+            ArrayList<Reward> rewards = new ArrayList<>();
+            while(rs.next()){
+                rewards.add(new Reward(
+                        rs.getInt(1),
+                        rs.getInt(2),
+                        rs.getString(3),
+                        rs.getDouble(4)));
+            }
+            response.setObject(rewards);
+
+            return response;
+
+        } catch (SQLException e) {
+            return new Response(false, "Can't find that reward!");
+        }
+    }
+
+    @Override
+    public Response createLevel(Level level) throws RemoteException {
+        Debug.m("Creating level! " + level.toString());
+
+        try {
+
+            statement.executeUpdate("INSERT INTO levels ( id_project, description, goal)" +
+                    "VALUES" +
+                    "(" + level.getIdProject() + ", '" +
+                    level.getDescription() + "', " +
+                    level.getGoal() + ");");
+
+            rs=statement.getGeneratedKeys();
+
+            Response response = new Response(true,"Level inserted successfully!");
+
+            if (rs.next()){
+                response.setObject(new Level(
+                        rs.getInt(1),
+                        rs.getInt(2),
+                        rs.getString(3),
+                        rs.getInt(4)));
+            }
+
+            return response;
+        }
+        catch (SQLException ex){
+            // handle any errors
+            Debug.m("SQLException: " + ex.getMessage());
+            Debug.m("SQLState: " + ex.getSQLState());
+            Debug.m("VendorError: " + ex.getErrorCode());
+            return new Response(false, "Error on Level insertion");
+        }
+        finally {
+            // cleaning stuff
+            if (rs != null) {
+                try {rs.close();} catch (SQLException sqlEx) { } // ignore
+
+                rs = null;
+            }
+
+            if (statement != null) {
+                try {statement.close();} catch (SQLException sqlEx) { } // ignore
+
+                statement = null;
+            }
+        }
+    }
+
+    @Override
+    public Response getLevels(int projectId) throws RemoteException {
+        try {
+            rs = statement.executeQuery("SELECT * FROM levels WHERE id_project = " + projectId);
+
+            Response response = new Response(true, "There is your level!");
+
+            ArrayList<Level> levels = new ArrayList<>();
+            while(rs.next()){
+                levels.add(new Level(
+                        rs.getInt(1),
+                        rs.getInt(2),
+                        rs.getString(3),
+                        rs.getInt(4)));
+            }
+            response.setObject(levels);
+
+            return response;
+
+        } catch (SQLException e) {
+            return new Response(false, "Can't find that level!");
+        }
+    }
+
+    @Override
+    public Response createPoll(Poll poll) throws RemoteException {
+        Debug.m("Creating poll! " + poll.toString());
+
+        try {
+
+            statement.executeUpdate("INSERT INTO polls ( title, description, id_project, answer1, answer2)" +
+                    "VALUES" +
+                    "('" + poll.getTitle() + "', '" +
+                    poll.getDescription() + "', " +
+                    poll.getIdProject() + ", '" +
+                    poll.getAnswer1() + "', '" +
+                    poll.getAnswer1() + "');");
+
+            rs=statement.getGeneratedKeys();
+
+            Response response = new Response(true,"Poll inserted successfully!");
+
+            if (rs.next()){
+                response.setObject(new Poll(
+                        rs.getInt("id"),
+                        rs.getString("title"),
+                        rs.getString("description"),
+                        rs.getInt("id_project"),
+                        rs.getString("answer1"),
+                        rs.getString("answer2")));
+            }
+
+            return response;
+        }
+        catch (SQLException ex){
+            // handle any errors
+            Debug.m("SQLException: " + ex.getMessage());
+            Debug.m("SQLState: " + ex.getSQLState());
+            Debug.m("VendorError: " + ex.getErrorCode());
+            return new Response(false, "Error on Poll insertion");
+        }
+        finally {
+            // cleaning stuff
+            if (rs != null) {
+                try {rs.close();} catch (SQLException sqlEx) { } // ignore
+
+                rs = null;
+            }
+
+            if (statement != null) {
+                try {statement.close();} catch (SQLException sqlEx) { } // ignore
+
+                statement = null;
+            }
+        }
+    }
+
+    @Override
+    public Response getPolls(int projectId) throws RemoteException {
+        try {
+            rs = statement.executeQuery("SELECT * FROM polls WHERE id_project = " + projectId);
+
+            Response response = new Response(true, "There is your poll!");
+
+            ArrayList<Poll> polls = new ArrayList<>();
+            while(rs.next()){
+                polls.add(new Poll(
+                        rs.getInt("id"),
+                        rs.getString("title"),
+                        rs.getString("description"),
+                        rs.getInt("id_project"),
+                        rs.getString("answer1"),
+                        rs.getString("answer2")));
+            }
+            response.setObject(polls);
+
+            return response;
+
+        } catch (SQLException e) {
+            return new Response(false, "Can't find any poll!");
+        }
+    }
+
+    @Override
+    public Response createVote(Vote vote) throws RemoteException {
+        Debug.m("Creating vote! " + vote.toString());
+
+        try {
+
+            statement.executeUpdate("INSERT INTO votes ( id_poll, answer_index)" +
+                    "VALUES" +
+                    "(" + vote.getIdPoll() + ", " +
+                    vote.getOption() + ");");
+
+            rs=statement.getGeneratedKeys();
+
+            Response response = new Response(true,"Vote inserted successfully!");
+
+            if (rs.next()){
+                response.setObject(new Vote(
+                        rs.getInt("id"),
+                        rs.getInt("id_poll"),
+                        rs.getInt("answer_index")));
+            }
+
+            return response;
+        }
+        catch (SQLException ex){
+            // handle any errors
+            Debug.m("SQLException: " + ex.getMessage());
+            Debug.m("SQLState: " + ex.getSQLState());
+            Debug.m("VendorError: " + ex.getErrorCode());
+            return new Response(false, "Error on Vote insertion");
+        }
+        finally {
+            // cleaning stuff
+            if (rs != null) {
+                try {rs.close();} catch (SQLException sqlEx) { } // ignore
+
+                rs = null;
+            }
+
+            if (statement != null) {
+                try {statement.close();} catch (SQLException sqlEx) { } // ignore
+
+                statement = null;
+            }
+        }
+    }
+
+    @Override
+    public Response getVotes(int pollId) throws RemoteException {
+        try {
+            rs = statement.executeQuery("SELECT * FROM votes WHERE id_poll = " + pollId);
+
+            Response response = new Response(true, "There is your votes!");
+
+            ArrayList<Vote> votes = new ArrayList<>();
+            while(rs.next()){
+                votes.add(new Vote(
+                        rs.getInt("id"),
+                        rs.getInt("id_poll"),
+                        rs.getInt("answer_index")));
+            }
+            response.setObject(votes);
+
+            return response;
+
+        } catch (SQLException e) {
+            return new Response(false, "Can't find any votes!");
+        }
     }
 }
