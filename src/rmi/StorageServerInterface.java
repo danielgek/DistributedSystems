6c543bb658a3c6c1c9eaedf96ec45d259027a4a6
danielgek <danielgek@gmail.com>
2015-10-22 02:03:28 +0100

    add new methods to rim interface

--- src/rmi/StorageServerInterface.java
+++ src/rmi/StorageServerInterface.java
@@ -12,20 +12,25 @@ import java.util.Date;
 public interface StorageServerInterface extends Remote{
     Response register(User user) throws RemoteException;
     Response login(User user) throws RemoteException;
+    Response getUser(int id) throws RemoteException;
     Response createProject(Project project) throws RemoteException;
     Response getProject(int id) throws RemoteException;
     Response getProjectByAdmin(int id) throws RemoteException;
     Response getProjects() throws RemoteException;
+    Response getOldProjects() throws RemoteException;
+    Response removeProject(int id) throws RemoteException;
     Response addReward(Reward reward) throws RemoteException;
     Response getRewards(int projectId) throws RemoteException;
-    Response addLevel(Level object) throws RemoteException;
+    Response getCurrentRewards(int projectId) throws RemoteException;
+    Response addLevel(Level level) throws RemoteException;
     Response getLevels(int projectId) throws RemoteException;
     Response addPoll(Poll poll) throws RemoteException;
     Response getPolls(int projectId) throws RemoteException;
     Response getPoll(int pollId) throws RemoteException;
     Response addVote(Vote vote) throws RemoteException;
     Response getVotes(int pollId) throws RemoteException;
-
-
-
+    Response pledge(Pledge pledge) throws RemoteException;
+    Response removeReward(int id) throws RemoteException;
+    Response sendMessage(Message message) throws RemoteException;
+    Response getMessagesByProject(int projectId) throws RemoteException;
 }
