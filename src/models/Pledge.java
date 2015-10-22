3fe5ae163e029f796ecf6289106ecd924b767280
danielgek <danielgek@gmail.com>
2015-10-22 02:00:35 +0100

    add pledge model

---
+++ src/models/Pledge.java
@@ -0,0 +1,52 @@
+package models;
+
+import java.io.Serializable;
+
+/**
+ * Created by danielgek on 17/10/15.
+ */
+public class Pledge implements Serializable{
+
+    private int projectId;
+    private int userId;
+    private double amount;
+
+    public Pledge(int projectId, int userId, double amount) {
+        this.projectId = projectId;
+        this.userId = userId;
+        this.amount = amount;
+    }
+
+    public int getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(int projectId) {
+        this.projectId = projectId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Pledge{" +
+                "projectId=" + projectId +
+                ", userId=" + userId +
+                ", amount=" + amount +
+                '}';
+    }
+}
