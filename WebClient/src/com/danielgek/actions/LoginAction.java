5c98f8a0406fab8d91587f3d2009c35ce7f5ff22
danielgek <danielgek@gmail.com>
2015-12-02 22:46:30 +0000

    create login action

--- WebClient/src/com/danielgek/actions/LoginAction.java
+++ WebClient/src/com/danielgek/actions/LoginAction.java
@@ -2,10 +2,14 @@ package com.danielgek.actions;
 
 import com.danielgek.models.UserRepository;
 import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.util.logging.Logger;
+import com.opensymphony.xwork2.util.logging.LoggerFactory;
 import models.User;
 import org.apache.struts2.interceptor.SessionAware;
+import org.apache.struts2.json.annotations.JSON;
 
 import java.util.Map;
+import java.util.logging.LogManager;
 
 /**
  * Created by danielgek on 28/11/15.
@@ -20,6 +24,11 @@ public class LoginAction extends ActionSupport implements SessionAware{
     private String username;
 
     private Map<String,Object> session;
+    private User user;
+
+
+
+
 
     /*
      * Creates the MessageStore model object and
@@ -34,35 +43,41 @@ public class LoginAction extends ActionSupport implements SessionAware{
 
         if(userRepository.authenticate(username,password)){
             session.put("user",userRepository.getUser());
+            user = userRepository.getUser();
             return SUCCESS;
         }
 
 
         return LOGIN;
     }
-
+    @JSON(serialize=false)
     public UserRepository getUserRepository() {
         return userRepository;
     }
-
+    @JSON(serialize=false)
     public void setUserRepository(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
-
+    @JSON(serialize=false)
     public void setPassword(String password) {
         this.password = password;
     }
 
+    @JSON(serialize=false)
     public String getPassword() {
         return password;
     }
-
+    @JSON(serialize=false)
     public void setUsername(String username) {
         this.username = username;
     }
-
+    @JSON(serialize=false)
     public String getUsername() {
         return username;
+    }
+
+    public User getUser(){
+        return this.user;
     }
 
     @Override
